d0569189e5d31edfb26237d539ebf0625b2bfd1b
Jacob Santamaría <jasa23128@gmail.com>
2026-08-29 17:23:55 -0500

    Create Meseros.java

---
+++ El_proyecto/Restaurante_Coso/Meseros.java
@@ -0,0 +1,25 @@
+public class Meseros implements Acciones_meseros {
+    private int id;
+    private int mesas_atendidas;
+
+    public Meseros(int id, int mesas_atendidas) {
+        this.id = id;
+        this.mesas_atendidas = mesas_atendidas;
+    };
+
+    @Override
+    public void asignar_mesa() {
+    };
+
+    @Override
+    public void atender_mesa() {
+    };
+
+    @Override
+    public void tomar_pedido() {
+    };
+
+    @Override
+    public void entregar_pedido() {
+    };
+};
