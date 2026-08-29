f9e8a47cf29cfd5bd593670480a086cdd9aa8368
Jacob Santamaría <jasa23128@gmail.com>
2026-08-29 17:03:09 -0500

    Create Mesas.java

---
+++ El_proyecto/Restaurante_Coso/Mesas.java
@@ -0,0 +1,15 @@
+public class Mesas{
+    private int numero;
+    private String cliente_mesa;
+    private boolean disponible;
+
+    public Mesas(int numero, String cliente_mesa, boolean disponible){
+        this.numero = numero;
+        this.cliente_mesa = cliente_mesa;
+        this.disponible = disponible;
+    };
+
+    public boolean disponibilidad_mesa(){
+        return disponible;
+    };
+};
