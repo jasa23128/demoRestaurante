0bf996d2a140051a016b67af7ccf5d1651beed37
matiasgarzonsanchez <matiasgarzonsanchez@gmail.com>
2026-08-30 16:41:23 -0500

    agregue metodos y manej ode errores

--- El_proyecto/Restaurante_Coso/Pedidos.java
+++ El_proyecto/Restaurante_Coso/Pedidos.java
@@ -1,37 +1,90 @@
 package Restaurante_Coso;
 
 public class Pedidos {
-    private Productos[] productos;
-    private static final int max_productos = 10;
-    private int cantidadProductos; //contador Trate de usar solo un tipo, o camelCase o snake_case, no los dos.
-    private boolean tipo_pedido; //puede hacer este Tipo_orden en vez de boolean, asi saca algo que no sea t o f.
+    private static int contador_pedidos = 0;
 
-    public Pedidos() {
-        productos = new Productos[max_productos];
-        cantidadProductos = 0;
+    private static final int max_productos = 10;
+
+    private int numero_pedido;
+    private Productos[] productos;
+    private int cantidad_productos;
+    private Tipo_orden tipo_pedido;
+    private boolean cerrado;
+
+    public Pedidos(Tipo_orden tipo_pedido) {
+        contador_pedidos++;
+        this.numero_pedido = contador_pedidos;
+        this.tipo_pedido = tipo_pedido;
+        this.productos = new Productos[max_productos];
+        this.cantidad_productos = 0;
+        this.cerrado = false;
     }
 
-    public boolean agregarProducto(Productos producto) {
-        try {
-            if (!producto.disponibilidad_producto()) {
-                throw new Exception_producto_agotado("No hay de eso, se acabó");
+    public boolean agregarProducto(Productos producto) throws Exception_producto_agotado, Exception_chef {
+        if (!producto.disponibilidad_producto()) {
+            if (producto instanceof Platos_de_comido) {
+                Platos_de_comido plato = (Platos_de_comido) producto;
+                if (plato.getEspecial_chef()) {
+                    throw new Exception_chef("El especial del chef '" + plato.getNombre() + "' ya se agotó");
+                }
             }
+            throw new Exception_producto_agotado("No hay de eso, se acabó");
+        }
 
-            if (cantidadProductos >= 10) {
-                System.out.println("El pedido está lleno, no se puede agregar más productos");
-                return false;
-            }
-
-            productos[cantidadProductos] = producto;
-            cantidadProductos++;
-            System.out.println("Producto agregado");
-            return true;
-
-        } catch (Exception_producto_agotado e) {
-            System.out.println(e.getMessage());
+        if (cantidad_productos >= max_productos) {
+            System.out.println("El pedido está lleno, no se puede agregar más productos");
             return false;
+        }
+
+        productos[cantidad_productos] = producto;
+        cantidad_productos++;
+        System.out.println("Producto agregado");
+        return true;
+    }
+
+    public void cerrarPedido() throws Exception_pedido_vacio {
+        if (cantidad_productos == 0) {
+            throw new Exception_pedido_vacio("El pedido #" + numero_pedido + " no tiene productos, no se puede cerrar");
+        }
+        this.cerrado = true;
+    }
+
+    public void validarNoDuplicado(Pedidos otro) throws Exception_equals {
+        if (this.equals(otro)) {
+            throw new Exception_equals("El pedido #" + numero_pedido + " ya existe, no se puede duplicar");
         }
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Pedidos otro = (Pedidos) obj;
+        return this.numero_pedido == otro.numero_pedido;
+    }
 
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(numero_pedido);
+    }
+
+    public boolean isCerrado() {
+        return cerrado;
+    }
+
+    public int getNumeroPedido() {
+        return numero_pedido;
+    }
+
+    public Tipo_orden getTipoPedido() {
+        return tipo_pedido;
+    }
+
+    public Productos[] getProductos() {
+        return productos;
+    }
+
+    public int getCantidadProductos() {
+        return cantidad_productos;
+    }
 }
