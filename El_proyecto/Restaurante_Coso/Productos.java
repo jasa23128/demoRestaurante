f9e7c680e0afec87fe9d980df0f78ef0c3aa58f0
Harold <galeanoalzateharoldandres@gmail.com>
2026-08-30 18:12:19 -0500

    implementacion de getters en producto

--- El_proyecto/Restaurante_Coso/Productos.java
+++ El_proyecto/Restaurante_Coso/Productos.java
@@ -1,16 +1,39 @@
 package Restaurante_Coso;
 
 public abstract class Productos {
+    protected String nombre;
     protected double precio;
     protected  int codigo;
     protected String tipo;
     protected boolean disponibilidad;
 
-    public Productos(int codigo, String tipo, boolean disponibilidad, double precio) {
+    public Productos(String nombre,int codigo, String tipo, boolean disponibilidad, double precio) {
+        this.nombre = nombre;
         this.codigo = codigo;
         this.tipo = tipo;
         this.disponibilidad = disponibilidad;
         this.precio = precio;
+    }
+
+    //getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
     }
 
     public abstract boolean disponibilidad_producto();
