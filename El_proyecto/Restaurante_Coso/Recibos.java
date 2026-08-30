e99141ff81e1dfd7354903122c54bc6a669c6fd6
Harold <galeanoalzateharoldandres@gmail.com>
2026-08-30 11:16:48 -0500

    clase Recibos modificado

--- El_proyecto/Restaurante_Coso/Recibos.java
+++ El_proyecto/Restaurante_Coso/Recibos.java
@@ -1,44 +1,45 @@
 package Restaurante_Coso;
-// libreria arrays
 import java.util.Arrays;
-//clase inmutable
+
+// Clase inmutable
 public final class Recibos {
+
     private final String[] infoCalculoPedido;
     private final double totalCalculado;
 
-    public Recibos(String[ ]infoCaculoPedido, double totalCalculo) {
-        if (infoCaculoPedido != null) {
-            this.infoCalculoPedido = Arrays.copyOf(infoCaculoPedido, infoCaculoPedido.length);
-        }else {
-            this.infoCalculoPedido = new String[0];
-        }
-        this.totalCalculado = totalCalculo;
+    public Recibos(String[] infoCalculoPedido, double totalCalculado) {
+        this.infoCalculoPedido = infoCalculoPedido != null
+                ? Arrays.copyOf(infoCalculoPedido, infoCalculoPedido.length)
+                : null;
+        this.totalCalculado = totalCalculado;
     }
 
-    public String[] getInfoCalculoPedido(){
-        return Arrays.copyOf(infoCalculoPedido, infoCalculoPedido.length);
+    // Getters
+    public String[] getInfoCalculoPedido() {
+        return infoCalculoPedido != null
+                ? Arrays.copyOf(infoCalculoPedido, infoCalculoPedido.length)
+                : null;
     }
 
-    public  double getTotalCalculado(){
+    public double getTotalCalculado() {
         return totalCalculado;
     }
 
     @Override
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("=====================================\n");
-        sb.append("                 RECIBO              \n");
-        sb.append("=====================================\n");
-        sb.append("Detalle del Pedido:\n");
+    public String toString() {
+        String textoRecibo = "===== RECIBO DE COMPRA =====\n";
 
-        for (String item : infoCalculoPedido){
-            sb.append("-").append(item).append("\n");
+        if (infoCalculoPedido != null) {
+            for (int i = 0; i < infoCalculoPedido.length; i++) {
+                textoRecibo += " - " + infoCalculoPedido[i] + "\n";
+            }
         }
-        sb.append("-------------------------------------\n");
-        sb.append(String.format("Total : $%.2f\n", totalCalculado));
-        sb.append("=====================================");
-        return sb.toString();
+
+        textoRecibo += "----------------------------\n";
+        textoRecibo += "Total a pagar: $" + totalCalculado + "\n";
+        textoRecibo += "============================";
+
+        return textoRecibo;
     }
-
-
 }
+
