254e2bf0df99015859599bbd7d054d848b3e71d6
matiasgarzonsanchez <matiasgarzonsanchez@gmail.com>
2026-08-30 16:14:43 -0500

    añadi manejo de errores

--- El_proyecto/Restaurante_Coso/Pedidos.java
+++ El_proyecto/Restaurante_Coso/Pedidos.java
@@ -1,37 +1,65 @@
 package Restaurante_Coso;
 
 public class Pedidos {
-    private Productos[] productos;
-    private static final int max_productos = 10;
-    private int cantidadProductos; //contador Trate de usar solo un tipo, o camelCase o snake_case, no los dos.
-    private boolean tipo_pedido; //puede hacer este Tipo_orden en vez de boolean, asi saca algo que no sea t o f.
+    private static int contador_pedidos = 0;
 
-    public Pedidos() {
-        productos = new Productos[max_productos];
-        cantidadProductos = 0;
+    private static final int max_productos = 10;
+
+    private int numero_pedido;
+    private Productos[] productos;
+    private int cantidad_productos;
+    private Tipo_orden tipo_pedido;
+    private boolean cerrado;
+
+    public Pedidos(Tipo_orden tipo_pedido) {
+        contador_pedidos++;
+        this.numero_pedido = contador_pedidos;
+        this.tipo_pedido = tipo_pedido;
+        this.productos = new Productos[max_productos];
+        this.cantidad_productos = 0;
+        this.cerrado = false;
     }
 
-    public boolean agregarProducto(Productos producto) {
-        try {
-            if (!producto.disponibilidad_producto()) {
-                throw new Exception_producto_agotado("No hay de eso, se acabó");
-            }
+    public boolean agregarProducto(Productos producto) throws Exception_producto_agotado {
+        if (!producto.disponibilidad_producto()) {
+            throw new Exception_producto_agotado("No hay de eso, se acabó");
+        }
 
-            if (cantidadProductos >= 10) {
-                System.out.println("El pedido está lleno, no se puede agregar más productos");
-                return false;
-            }
-
-            productos[cantidadProductos] = producto;
-            cantidadProductos++;
-            System.out.println("Producto agregado");
-            return true;
-
-        } catch (Exception_producto_agotado e) {
-            System.out.println(e.getMessage());
+        if (cantidad_productos >= max_productos) {
+            System.out.println("El pedido está lleno, no se puede agregar más productos");
             return false;
         }
+
+        productos[cantidad_productos] = producto;
+        cantidad_productos++;
+        System.out.println("Producto agregado");
+        return true;
     }
 
+    public void cerrarPedido() throws Exception_pedido_vacio {
+        if (cantidad_productos == 0) {
+            throw new Exception_pedido_vacio("El pedido #" + numero_pedido + " no tiene productos, no se puede cerrar");
+        }
+        this.cerrado = true;
+    }
 
+    public boolean isCerrado() {
+        return cerrado;
+    }
+
+    public int getNumeroPedido() {
+        return numero_pedido;
+    }
+
+    public Tipo_orden getTipoPedido() {
+        return tipo_pedido;
+    }
+
+    public Productos[] getProductos() {
+        return productos;
+    }
+
+    public int getCantidadProductos() {
+        return cantidad_productos;
+    }
 }
