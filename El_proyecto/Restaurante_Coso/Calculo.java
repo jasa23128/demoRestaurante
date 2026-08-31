5d7874ad4a992651355963f3d79492cea5eb7839
matiasgarzonsanchez <matiasgarzonsanchez@gmail.com>
2026-08-31 05:53:47 -0500

    Update Calculo.java
    Added methods to obtain product names and find the most ordered product.

--- El_proyecto/Restaurante_Coso/Calculo.java
+++ El_proyecto/Restaurante_Coso/Calculo.java
@@ -4,6 +4,8 @@ public class Calculo {
 
     public static final double PORCENTAJE_PROPINA = 10.0;
 
+    private static final int MAX_PRODUCTOS_DISTINTOS = 50;
+
     private Calculo() {}
 
     public static double calcularSubtotal(Pedidos pedido) {
@@ -29,5 +31,54 @@ public class Calculo {
         double subtotal = calcularSubtotal(pedido);
         double propina = calcularPropina(subtotal, pedido.getTipoPedido());
         return subtotal + propina;
+    }
+
+    private static String obtenerNombre(Productos producto) {
+        if (producto instanceof Platos_de_comido) {
+            return ((Platos_de_comido) producto).getNombre();
+        } else if (producto instanceof Bebidas) {
+            return ((Bebidas) producto).getNombre();
+        }
+        return "Producto";
+    }
+
+    public static String productoMasPedido(Pedidos[] pedidos, int cantidadPedidos) {
+        String[] nombres = new String[MAX_PRODUCTOS_DISTINTOS];
+        int[] veces = new int[MAX_PRODUCTOS_DISTINTOS];
+        int cantidadDistintos = 0;
+
+        for (int i = 0; i < cantidadPedidos; i++) {
+            Productos[] productos = pedidos[i].getProductos();
+            int cantidadEnPedido = pedidos[i].getCantidadProductos();
+
+            for (int j = 0; j < cantidadEnPedido; j++) {
+                String nombre = obtenerNombre(productos[j]);
+                boolean encontrado = false;
+
+                for (int k = 0; k < cantidadDistintos; k++) {
+                    if (nombres[k].equals(nombre)) {
+                        veces[k]++;
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado && cantidadDistintos < MAX_PRODUCTOS_DISTINTOS) {
+                    nombres[cantidadDistintos] = nombre;
+                    veces[cantidadDistintos] = 1;
+                    cantidadDistintos++;
+                }
+            }
+        }
+
+        int maxVeces = 0;
+        String masPedido = "Ninguno";
+        for (int i = 0; i < cantidadDistintos; i++) {
+            if (veces[i] > maxVeces) {
+                maxVeces = veces[i];
+                masPedido = nombres[i];
+            }
+        }
+
+        return masPedido + " (" + maxVeces + " veces)";
     }
 }
