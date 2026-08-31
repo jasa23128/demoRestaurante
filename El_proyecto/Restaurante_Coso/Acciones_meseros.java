832608ea941eccc3ff4e11c1606ea188beb0f8c1
matiasgarzonsanchez <matiasgarzonsanchez@gmail.com>
2026-08-31 05:55:36 -0500

    Update Acciones_meseros.java

--- El_proyecto/Restaurante_Coso/Acciones_meseros.java
+++ El_proyecto/Restaurante_Coso/Acciones_meseros.java
@@ -1,7 +1,7 @@
 package Restaurante_Coso;
 
 public interface Acciones_meseros {
-    void tomar_pedido(Pedidos pedido, Productos producto) throws Exception_producto_agotado;
+    void tomar_pedido(Pedidos pedido, Productos producto) throws Exception_producto_agotado, Exception_chef;
     void entregar_pedido(Pedidos pedido) throws Exception_pedido_vacio;
     void asignar_mesa(Mesas mesa, String cliente) throws Exception_mesa_ocupada;
     void atender_mesa(Mesas mesa);
